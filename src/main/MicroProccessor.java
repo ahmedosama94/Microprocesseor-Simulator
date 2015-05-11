f3d1455a0ca9cbfe4d293c8d4fe3738be5d37e92
miada nour <miadanour@gmail.com>
2015-05-11 09:20:56 +0200

    micro instructions that i am not sure of

--- src/main/MicroProccessor.java
+++ src/main/MicroProccessor.java
@@ -1,9 +1,17 @@
 package main;
 
+import hardware.components.ALU;
+import hardware.components.ALUopr;
 import hardware.components.Register;
+import hardware.components.units.ALUUnit;
+import hardware.components.units.AddressUnit;
+import hardware.components.units.RegisterMemory;
 
 public class MicroProccessor {
-	boolean RSEL,Write,EB,EA,EXT,EDH,EDL,DOH,DOL,EIR,AOH,AOL,CAD,ESP,EPC,EOAR,NAD,EAH,EAL;
+	boolean RSEL,Write,EB,EA,EXT,EDH,EDL,DOH,DOL,EIR,AOH,AOL,CAD,ESP,EPC,EOAR,NAD,EAH,EAL,ERR;
+	ALUUnit alu;
+	AddressUnit addunit;
+	
 	public MicroProccessor(){
 		
 	}
@@ -13,7 +21,22 @@ public class MicroProccessor {
 		EDH = true;
 		EDL =true;
 		EB=true;
+	
 		Register.clockCycleAll();
+		
+		alu.getALU().setSelect(ALUopr.B);
+		ERR =true;
+		DOH=true;
+		DOL=true;
+		RSEL=true;
+		
+		Register.clockCycleAll();
+	
+	}
+	
+	public void ADDtomemory(){
+		EDL=true;
+		EDH=true;
 		
 	}
 	
